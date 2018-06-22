b059c30133d1dfaeee246a9c26664692ced08b2b
Marius Lazar <mariusl@packtpub.com>
2018-06-22 17:34:58 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function (req, res) {
+    res.send('Hello');
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
+});
