3b6dc33efc88c4611467a74dd7196f5c02bf11a1
Marius Lazar <mariusl@packtpub.com>
2018-06-22 18:25:25 +0100

    added verify route

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.post('/register', (req, res) => {
     // A user registers with a mobile phone number
     let phoneNumber = req.body.number;
     console.log(phoneNumber);
-    const payload = {number: phoneNumber, brand: 'Marius Company'};
+    const payload = {number: phoneNumber, brand: 'Mare Boss Company'};
     nexmo.verify.request(payload, (err, result) => {
       console.log('status:', result.status);
       if(err) {
@@ -38,11 +38,32 @@ app.post('/register', (req, res) => {
       } else {
         console.log('Success verify');
         let requestId = result.request_id;
+        console.log('requestId:', requestId);
         if(result.status == '0') {
           res.render('verify', {requestId: requestId});
           // Success! Now, have your user enter the PIN
         } else {
           res.status(401).send(result.error_text);
+        }
+      }
+    });
+  });
+
+  app.post('/verify', (req, res) => {
+    let pin = req.body.pin;
+    let requestId = req.body.requestId;
+
+    nexmo.verify.check({request_id: requestId, code: pin}, (err, result) => {
+      if(err) {
+        console.log('err:', err)
+        // handle the error
+      } else {
+        console.log('status verify: ', result.status);
+        if(result && result.status == '0') { // Success!
+          res.status(200).send('Account verified!');
+          res.render('status', {message: 'Account verified! 🎉'});
+        } else {
+          // handle the error - e.g. wrong PIN
         }
       }
     });
