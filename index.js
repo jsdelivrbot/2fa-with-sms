87ea607e4305b701e9efa43add59a9f1d97f4d7e
Marius Lazar <mariusl@packtpub.com>
2018-06-24 19:48:12 +0100

    added endpoints for frontend call

--- index.js
+++ index.js
@@ -49,6 +49,30 @@ app.post('/register', (req, res) => {
     });
   });
 
+  app.post('/register2', (req, res) => {
+    // A user registers with a mobile phone number
+    let phoneNumber = req.body.number;
+    console.log(phoneNumber);
+    const payload = {number: phoneNumber, brand: 'Mare Boss Company'};
+    nexmo.verify.request(payload, (err, result) => {
+      console.log('status:', result.status);
+      if(err) {
+        console.log('Verify Error: ', err);
+        res.sendStatus(500);
+      } else {
+        console.log('Success verify');
+        let requestId = result.request_id;
+        console.log('requestId:', requestId);
+        if(result.status == '0') {
+          res.status(200).send({requestId: requestId});
+          // Success! Now, have your user enter the PIN
+        } else {
+          res.status(401).send(result.error_text);
+        }
+      }
+    });
+  });
+
   app.post('/verify', (req, res) => {
     console.log('verify body: ', req.body);
     let pin = req.body.pin;
@@ -64,6 +88,28 @@ app.post('/register', (req, res) => {
           res.status(200).send('Account verified!');
           res.render('status', {message: 'Account verified! 🎉'});
         } else {
+          // handle the error - e.g. wrong PIN
+        }
+      }
+    });
+  });
+
+  app.post('/verify2', (req, res) => {
+    console.log('verify body: ', req.body);
+    let pin = req.body.pin;
+    let requestId = req.body.requestId;
+
+    nexmo.verify.check({request_id: requestId, code: pin}, (err, result) => {
+      if(err) {
+        console.log('err:', err)
+        // handle the error
+      } else {
+        console.log('status verify: ', result.status);
+        if(result && result.status == '0') { // Success!
+          res.status(200).send('Account verified!');
+        //   res.render('status', {message: 'Account verified! 🎉'});
+        } else {
+            res.status(400).send('Account not verified!');
           // handle the error - e.g. wrong PIN
         }
       }
